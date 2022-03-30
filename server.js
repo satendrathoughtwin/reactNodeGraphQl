808bd238f8d918aba73ddf488bd05194f056715a
satendraSahu <satendra.thoughtwin@gmail.com>
2022-03-30 17:09:38 +0530

    2_3_4_5 th tutorial

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { quotes, users } from "./src/service/fakeDb.js";
 
 // create new schema
 
@@ -9,14 +10,35 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-co
 //     3. resolver --> it will manage both query and mutation, will perform logical operations
 const typeDefs = gql`
   type Query {
-    greet: String
+    users : [User]
+    user(id : ID!) : User
+    quotes : [Quote]
+    userQuote(by :ID!) : [Quote]
+  }
+  type User {
+      id : ID
+      firstName : String
+      lastName : String
+      email : String
+      password : String
+      quotes :  [Quote]
+  }
+  type Quote {
+      name : String
+      by : ID
   }
 `;
 
 const resolvers = {
   Query: {
-    greet: () => "Hello world",
+   users : ()=>users,
+   user : (_,args)=>users.find(userdata=>userdata.id == args.id),
+   quotes : ()=>quotes,
+   userQuote : (_,args)=>quotes.filter(userQote=> userQote.by == args.by)
   },
+  User :{
+    quotes : (usr)=> quotes.filter(quote =>quote.by == usr.id)
+  }
 };
 
 // create instance of apollo server
